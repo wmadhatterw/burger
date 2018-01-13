bb61defb996581dc704e78e49f3253e3b9556ab5
Seth Walton <sdwalton83@gmail.com>
2018-01-13 10:38:01 -0600

    final

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,5 +2,5 @@ INSERT INTO burgers (name) VALUES ('BK');
 INSERT INTO burgers (name) VALUES ('MCD');
 INSERT INTO burgers (name, munchy) VALUES ('JackinTheBox', true);
 INSERT INTO burgers (name, munchy) VALUES ('Hardees', true);
-INSERT INTO burgers (name, munchy) VALUES ('Sonic', true);
+INSERT INTO burgers (name, munchy) VALUES ('Cozy INN', true);
 INSERT INTO burgers (name) VALUES ('FiveGuys');
