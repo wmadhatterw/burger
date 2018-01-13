761c797c4d178d28a03ba8d8f2b7c67233e01986
Seth Walton <sdwalton83@gmail.com>
2018-01-12 21:50:22 -0600

    heroku test

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,14 @@ var connection = mysql.createConnection({
   password: "",
   database: "burger_db"
 });
-
+if(process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'hacktheplanet'
+  database: 'todoagain_db'
+})
 // Make connection.
 connection.connect(function(err) {
   if (err) {
