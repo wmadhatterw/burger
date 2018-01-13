7be16cd79b91d7d577e6a56ce0f00a4ffb4aab54
Seth Walton <sdwalton83@gmail.com>
2018-01-13 01:47:17 -0600

    maybe

--- config/connection.js
+++ config/connection.js
@@ -1,34 +1,57 @@
-// Set up MySQL connection.
-var mysql = require("mysql");
+var mysql = require('mysql');
+
 var connection;
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "burger_db"
-});
-// if(process.env.JAWSDB_URL) {
-//   connection = mysql.createConnection(process.env.JAWSDB_URL)
-// } else connection = mysql.createConnection({
-//   host: 'localhost',
-//   user: 'root',
-//   password: 'hacktheplanet'
-//   database: 'todoagain_db'
-// })
-// Make connection.
-// var mysql = require('mysql');
-// var connection = mysql.createConnection(process.env.JAWSDB_URL);
 
+if(process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else{
+    connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: null,
+        database: 'burger_database'
+    });
+}
 
-
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
-
-// Export connection for our ORM to use.
 module.exports = connection;
+
+
+
+
+
+
+// Set up MySQL connection.
+// var mysql = require("mysql");
+// var connection;
+// var connection = mysql.createConnection({
+//   port: 3306,
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   database: "burger_db"
+// });
+// // if(process.env.JAWSDB_URL) {
+// //   connection = mysql.createConnection(process.env.JAWSDB_URL)
+// // } else connection = mysql.createConnection({
+// //   host: 'localhost',
+// //   user: 'root',
+// //   password: 'hacktheplanet'
+// //   database: 'todoagain_db'
+// // })
+// // Make connection.
+// // var mysql = require('mysql');
+// // var connection = mysql.createConnection(process.env.JAWSDB_URL);
+
+
+
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });
+
+// // Export connection for our ORM to use.
+// module.exports = connection;
